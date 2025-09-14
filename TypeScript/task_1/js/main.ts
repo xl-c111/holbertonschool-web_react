df94c8db47060c4f0263bfe570e18e3151bb750d
xl-c111 <10430@holbertonstudents.com>
2025-09-14 21:58:28 +1000

    Write a Class named StudentClass

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,6 +11,32 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+const teacher1: Teacher = {
+  firstName: 'Amy',
+  lastName: 'Brown',
+  fullTimeEmployee: true,
+  location: 'Sydney',
+  yearsOfExperience: 12
+};
+const teacher2: Teacher = {
+  firstName: 'Bill',
+  lastName: 'Gates',
+  fullTimeEmployee: true,
+  location: 'Melbourne',
+  contract: true
+};
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false
+};
+
+console.log(teacher1);
+console.log(teacher2);
+console.log(teacher3);
+
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
@@ -20,3 +46,29 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
 };
 
 console.log(printTeacher('John', 'Doe'));
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(
+    private firstName: string,
+    private lastName: string
+  ) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
+const student: StudentClassInterface = new StudentClass('Amy', 'Brown');
+console.log(student.displayName());
+console.log(student.workOnHomework());
